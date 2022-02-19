d69ca198424adf9274e2f532f7635f2daa632476
Caseylin <capricornlin1989@gmail.com>
2022-02-19 20:46:51 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.html", "./src/**/*.js"],
+  // mode: "jit",
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: "#4f3cc9",
+        "primary-dark": "#4232aa",
+        secondary: "#ffffff",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/forms")],
+};
